f004db2c6310efbba547e9fa0a6384e452605741
jacknicklenson <jacknicklenson59@gmail.com>
2024-07-23 02:30:50 +0300

    initial gif support added

--- src/root.zig
+++ src/root.zig
@@ -7,13 +7,22 @@ pub const Rendered = union(enum) {
     multi_frames: []zigimg.Image,
 };
 
-fn parse_anim() Rendered {
-    @panic("Unimplemented!");
+fn parse_anim(alloc: std.mem.Allocator, img: zigimg.Image) !Rendered {
+    var frame = std.ArrayList(zigimg.Image).init(alloc);
+    for (img.animation.frames.items) |f| {
+        var grayscale_img = try zigimg.Image.create(alloc, img.width, img.height, .rgb24);
+        defer grayscale_img.deinit();
+        for (f.pixels.rgb24, 0..) |p, idx| {
+            grayscale_img.pixels.rgb24[idx] = zigimg.color.Rgb24.initRgb(p.g, p.g, p.g);
+            try frame.append(grayscale_img);
+        }
+    }
+    return .{ .multi_frames = try frame.toOwnedSlice() };
 }
 
 pub fn tuify(alloc: std.mem.Allocator, img: zigimg.Image) !Rendered {
     if (img.isAnimation()) {
-        return parse_anim();
+        return parse_anim(alloc, img);
     } else {
         var grayscale_img = try zigimg.Image.create(alloc, img.width, img.height, .rgb24);
         for (img.pixels.rgb24, 0..) |p, idx| {
@@ -25,42 +34,89 @@ pub fn tuify(alloc: std.mem.Allocator, img: zigimg.Image) !Rendered {
 }
 
 pub fn render(alloc: std.mem.Allocator, win: vaxis.Window, img: zigimg.Image, tuified: Rendered, tty: vaxis.Tty, vx: *vaxis.Vaxis) !void {
-    const w = win.width;
-    const h = win.height;
-    const cw = img.width / w;
-    const ch = img.height / h;
-    const total_cell_pixel = cw * ch;
-    var g = std.ArrayList(std.ArrayList(usize)).init(alloc);
-    defer {
-        for (0..h) |j| g.items[j].deinit();
-        g.deinit();
+    switch (tuified) {
+        .multi_frames => |mf| {
+            const w = win.width;
+            const h = win.height;
+            const cw = mf[0].width / w;
+            const ch = mf[0].height / h;
+            const total_cell_pixel = cw * ch;
+            var g = std.ArrayList(std.ArrayList(usize)).init(alloc);
+            defer {
+                for (0..h) |j| g.items[j].deinit();
+                g.deinit();
+            }
+            for (0..h) |j| {
+                try g.append(std.ArrayList(usize).init(alloc));
+                for (0..w) |_| {
+                    try g.items[j].append(0);
+                }
+            }
+            for (mf) |simg| {
+                for (simg.pixels.rgb24, 0..) |p, idx| {
+                    const y = idx / img.width;
+                    const cy = y / ch;
+                    const cx = (idx % img.width) / cw;
+                    g.items[cy % h].items[cx % w] += p.r;
+                }
+                win.clear();
+                var segments = std.ArrayList(vaxis.Segment).init(alloc);
+                defer segments.deinit();
+                for (0..h) |y| {
+                    for (0..w) |x| {
+                        g.items[y].items[x] /= total_cell_pixel;
+                        const avg = g.items[y].items[x];
+                        const style: vaxis.Style = .{
+                            .fg = .{ .rgb = [3]u8{ @truncate(avg), @truncate(avg), @truncate(avg) } },
+                            .bg = .{ .rgb = [3]u8{ 0, 0, 0 } },
+                        };
+                        try segments.append(.{ .style = style, .text = "·" });
+                    }
+                }
+                _ = try win.print(segments.items, .{});
+                try vx.render(tty.anyWriter());
+                std.time.sleep(100_000_000);
+            }
+        },
+        .single_frame => |simg| {
+            const w = win.width;
+            const h = win.height;
+            const cw = simg.width / w;
+            const ch = simg.height / h;
+            const total_cell_pixel = cw * ch;
+            var g = std.ArrayList(std.ArrayList(usize)).init(alloc);
+            defer {
+                for (0..h) |j| g.items[j].deinit();
+                g.deinit();
+            }
+            for (0..h) |j| {
+                try g.append(std.ArrayList(usize).init(alloc));
+                for (0..w) |_| {
+                    try g.items[j].append(0);
+                }
+            }
+            for (simg.pixels.rgb24, 0..) |p, idx| {
+                const y = idx / img.width;
+                const cy = y / ch;
+                const cx = (idx % img.width) / cw;
+                g.items[cy % h].items[cx % w] += p.r;
+            }
+            win.clear();
+            var segments = std.ArrayList(vaxis.Segment).init(alloc);
+            defer segments.deinit();
+            for (0..h) |y| {
+                for (0..w) |x| {
+                    g.items[y].items[x] /= total_cell_pixel;
+                    const avg = g.items[y].items[x];
+                    const style: vaxis.Style = .{
+                        .fg = .{ .rgb = [3]u8{ @truncate(avg), @truncate(avg), @truncate(avg) } },
+                        .bg = .{ .rgb = [3]u8{ 0, 0, 0 } },
+                    };
+                    try segments.append(.{ .style = style, .text = "·" });
+                }
+            }
+            _ = try win.print(segments.items, .{});
+            try vx.render(tty.anyWriter());
+        },
     }
-    for (0..h) |j| {
-        try g.append(std.ArrayList(usize).init(alloc));
-        for (0..w) |_| {
-            try g.items[j].append(0);
-        }
-    }
-    for (tuified.single_frame.pixels.rgb24, 0..) |p, idx| {
-        const y = idx / img.width;
-        const cy = y / ch;
-        const cx = (idx % img.width) / cw;
-        g.items[cy % h].items[cx % w] += p.r;
-    }
-    win.clear();
-    var segments = std.ArrayList(vaxis.Segment).init(alloc);
-    defer segments.deinit();
-    for (0..h) |y| {
-        for (0..w) |x| {
-            g.items[y].items[x] /= total_cell_pixel;
-            const avg = g.items[y].items[x];
-            const style: vaxis.Style = .{
-                .fg = .{ .rgb = [3]u8{ @truncate(avg), @truncate(avg), @truncate(avg) } },
-                .bg = .{ .rgb = [3]u8{ 0, 0, 0 } },
-            };
-            try segments.append(.{ .style = style, .text = "·" });
-        }
-    }
-    _ = try win.print(segments.items, .{});
-    try vx.render(tty.anyWriter());
 }
